cf834deb29a9d4a83b26be3e9beb51a8bad90d3c
alefels21-ale <alefe.ls21@gmail.com>
2025-08-01 20:34:58 +0200

    Update WeatherPanel.js

--- components/WeatherPanel.js
+++ components/WeatherPanel.js
@@ -5,7 +5,7 @@ const WeatherPanel = ({ latitude, longitude }) => {
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState(null);
 
-  const API_KEY = 'a7bb4ff922b4b83f7007702c5d30ddc5'; // ← Trocar pela sua chave da OpenWeather
+  const API_KEY = 'a7bb4ff922b4b83f7007702c5d30ddc5';//
 
   useEffect(() => {
     if (!latitude || !longitude) return;
