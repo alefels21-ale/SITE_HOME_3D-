db1d01be12b5a48c954a71043bc4b459010d393f
alefels21-ale <alefe.ls21@gmail.com>
2025-08-01 22:37:45 +0200

    Delete pages/globo.js

--- pages/globo.js
+++
@@ -1,8 +0,0 @@
-'use client'
-import dynamic from 'next/dynamic'
-
-const Globo3D = dynamic(() => import('../app/Globo3D'), { ssr: false })
-
-export default function GloboPage() {
-  return <Globo3D />
-}
