aba27727fdcc88d454751fe9da1c13121a2ddc73
alefels21-ale <alefe.ls21@gmail.com>
2025-08-01 23:20:10 +0200

    Update EarthSphere.js

--- components/EarthSphere.js
+++ components/EarthSphere.js
@@ -1,14 +1,15 @@
+'use client';
+
 import { useLoader } from '@react-three/fiber';
 import { TextureLoader } from 'three';
-import { Sphere } from '@react-three/drei';
 
 export default function EarthSphere() {
-  const texture = useLoader(TextureLoader, '/textures/earth_daymap.jpg');
+  const earthTexture = useLoader(TextureLoader, '/textures/earth_daymap.jpg');
 
   return (
     <mesh>
-      <sphereGeometry args={[1.5, 64, 64]} />
-      <meshStandardMaterial map={texture} />
+      <sphereGeometry args={[2, 64, 64]} />
+      <meshStandardMaterial map={earthTexture} />
     </mesh>
   );
 }
